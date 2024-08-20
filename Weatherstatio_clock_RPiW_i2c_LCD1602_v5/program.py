f536fb02ef52905032e56f18ce67beca4e36ce41
Nicu FLORICA (niq_ro) <nicu.florica@gmail.com>
2024-08-20 19:23:33 +0300

    Add files via upload

---
+++ Weatherstatio_clock_RPiW_i2c_LCD1602_v5/program.py
@@ -0,0 +1,165 @@
+import urequests
+import network
+import time
+from machine import Pin, WDT
+
+wdt=WDT(timeout=8000)  #https://docs.micropython.org/en/latest/library/machine.WDT.html
+
+led = Pin('LED', Pin.OUT)
+
+led.value(1)
+ 
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect("bbk2","internet2")
+openweather_api_key = "07ceb123258e85768ac5109b91012441"
+
+# init LCD
+from machine import I2C, Pin
+from lcd_api import LcdApi
+from pico_i2c_lcd import I2cLcd
+
+I2C_ADDR     = 39
+I2C_NUM_ROWS = 2
+I2C_NUM_COLS = 16
+
+i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)
+lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
+
+ #   lcd.clear()
+lcd.move_to(0,0)
+lcd.putstr("Openweathermap 5")
+lcd.move_to(1,1)
+lcd.putstr("Clock by niq_ro")
+time.sleep(5)
+lcd.clear()
+
+# connect the network       
+wait = 10
+while wait > 0:
+    if wlan.status() < 0 or wlan.status() >= 3:
+        break
+    wait -= 1
+    print('waiting for connection...')
+    time.sleep(1)
+ 
+# Handle connection error
+if wlan.status() != 3:
+    raise RuntimeError('wifi connection failed')
+else:
+    print('connected')
+    ip=wlan.ifconfig()[0]
+    print('IP: ', ip)
+ 
+# syncing the time
+import ntptime
+while True:
+    try:
+        ntptime.settime()
+        print('Time Set Successfully')
+        break
+    except OSError:
+        print('Time Setting...')
+        continue
+ 
+ 
+# Open Weather
+TEMPERATURE_UNITS = {
+    "standard": "K",
+    "metric": "C",
+    "imperial": "F",
+}
+ 
+SPEED_UNITS = {
+    "standard": "m/s",
+    "metric": "m/s",
+    "imperial": "mph",
+}
+ 
+units = "metric"
+ 
+def get_weather(city, api_key, units='metric', lang='en'):
+    '''
+    Get weather data from openweathermap.org
+        city: City name, state code and country code divided by comma, Please, refer to ISO 3166 for the state codes or country codes. https://www.iso.org/obp/ui/#search
+        api_key: Your unique API key (you can always find it on your openweather account page under the "API key" tab https://home.openweathermap.org/api_keys
+        unit: Units of measurement. standard, metric and imperial units are available. If you do not use the units parameter, standard units will be applied by default. More: https://openweathermap.org/current#data
+        lang: You can use this parameter to get the output in your language. More: https://openweathermap.org/current#multi
+    '''
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units={units}&lang={lang}"
+    print(url)
+    res = urequests.post(url)
+    return res.json()
+ 
+def print_weather(weather_data):
+    print(f'Timezone: {int(weather_data["timezone"] / 3600)}')
+    sunrise = time.localtime(weather_data['sys']['sunrise']+weather_data["timezone"])
+    sunset = time.localtime(weather_data['sys']['sunset']+weather_data["timezone"])
+    print(f'Sunrise: {sunrise[3]}:{sunrise[4]}')
+    print(f'Sunset: {sunset[3]}:{sunset[4]}')   
+    print(f'Country: {weather_data["sys"]["country"]}')
+    print(f'City: {weather_data["name"]}')
+    print(f'Coordination: [{weather_data["coord"]["lon"]}, {weather_data["coord"]["lat"]}')
+    print(f'Visibility: {weather_data["visibility"]}m')
+    print(f'Weather: {weather_data["weather"][0]["main"]}')
+    print(f'Temperature: {weather_data["main"]["temp"]}°{TEMPERATURE_UNITS[units]}')
+    print(f'Temperature min: {weather_data["main"]["temp_min"]}°{TEMPERATURE_UNITS[units]}')
+    print(f'Temperature max: {weather_data["main"]["temp_max"]}°{TEMPERATURE_UNITS[units]}')
+    print(f'Temperature feels like: {weather_data["main"]["feels_like"]}°{TEMPERATURE_UNITS[units]}')
+    print(f'Humidity: {weather_data["main"]["humidity"]}%')
+    print(f'Pressure: {weather_data["main"]["pressure"]}hPa')
+    print(f'Wind speed: {weather_data["wind"]["speed"]}{SPEED_UNITS[units]}')
+    #print(f'Wind gust: {weather_data["wind"]["gust"]}{SPEED_UNITS[units]}')
+    print(f'Wind direction: {weather_data["wind"]["deg"]}°')
+    if "clouds" in weather_data:
+        print(f'Cloudiness: {weather_data["clouds"]["all"]}%')
+    elif "rain" in weather_data:
+        print(f'Rain volume in 1 hour: {weather_data["rain"]["1h"]}mm')
+        print(f'Rain volume in 3 hour: {weather_data["rain"]["3h"]}mm')
+    elif "snow" in weather_data:
+        print(f'Snow volume in 1 hour: {weather_data["snow"]["1h"]}mm')
+        print(f'Snow volume in 3 hour: {weather_data["snow"]["3h"]}mm')    
+ 
+while True:
+    #wdt.feed()
+    # get weather
+    weather_data = get_weather('Craiova', openweather_api_key, units=units)
+    weather=weather_data["weather"][0]["main"]
+    t=weather_data["main"]["temp"]
+    rh=weather_data["main"]["humidity"]
+ 
+    # get time
+    hours=(time.localtime()[3]+int(weather_data["timezone"] / 3600))%24
+    mins=time.localtime()[4]
+    secs=time.localtime()[5]
+    
+    # shell print
+    print(f'Time: {hours}:{mins}:{secs}')
+    print_weather(weather_data)
+ 
+    # LCD print
+    #lcd.clear()
+    lcd.move_to(6,0)
+    lcd.putstr(".....     ")
+    lcd.move_to(0,1)
+    lcd.putstr("                ")
+
+    #time.sleep_ms(200)
+    string = f'{hours:02d}:{mins:02d} {weather}'
+    lcd.move_to(0,0)
+    lcd.putstr(string)
+    string = f'{t}{chr(223)}{TEMPERATURE_UNITS[units]} {rh}%RH'
+    lcd.move_to(0,1)
+    lcd.putstr(string)
+   
+    for y in range(0, (60-secs)/2):
+      wdt.feed()
+      lcd.move_to(2,0)
+      lcd.putstr(" ")
+      led.value(0)
+      time.sleep(1)
+      lcd.move_to(2,0)
+      lcd.putstr(":")
+      led.value(1)
+      time.sleep(1)
+    
